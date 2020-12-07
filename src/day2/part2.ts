6ef0ae733c872a9b35e32178b14598b13a26791d
Jonathan Darrer <jonny@jondarrer.me.uk>
2020-12-07 19:06:38 +0000

    2-2 :sparkles: Day 2 part 2 completed!

--- src/day2/part2.ts
+++ src/day2/part2.ts
@@ -1,9 +1,31 @@
 import { PolicyAndPassword } from './policy-and-password';
 import { Policy } from './policy';
 
-const part2 = (input: Array<PolicyAndPassword>): number => 0;
+const part2 = (list: Array<PolicyAndPassword>): number =>
+  list.reduce<number>(
+    (acc, cur) => acc + (isPasswordValid(cur.policy, cur.password) ? 1 : 0),
+    0
+  );
 
-const isPasswordValid = (policy: Policy, password: string): boolean => false;
+const isPasswordValid = (policy: Policy, password: string): boolean => {
+  if (
+    password.charAt(policy.min - 1) === policy.char &&
+    password.charAt(policy.max - 1) === policy.char
+  ) {
+    return false;
+  } else if (
+    password.charAt(policy.min - 1) !== policy.char &&
+    password.charAt(policy.max - 1) === policy.char
+  ) {
+    return true;
+  } else if (
+    password.charAt(policy.min - 1) === policy.char &&
+    password.charAt(policy.max - 1) !== policy.char
+  ) {
+    return true;
+  }
+  return false;
+};
 
 export default part2;
 export { isPasswordValid };
