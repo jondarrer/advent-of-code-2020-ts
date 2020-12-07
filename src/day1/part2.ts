fe31b2a41d058d28330052f4bb8c4466749beda1
Jonathan Darrer <jonny@jondarrer.me.uk>
2020-12-07 12:53:57 +0000

    1-2 :sparkles: Day 1 part 2 completed!

--- src/day1/part2.ts
+++ src/day1/part2.ts
@@ -1,9 +1,28 @@
-const part2 = (expenseReport: Array<number>): number => 0;
+const part2 = (expenseReport: Array<number>): number => {
+  const twoNumbers = whichThreeNumbersAddUpTo(expenseReport, 2020);
+  return twoNumbers[0] * twoNumbers[1] * twoNumbers[2];
+};
 
 const whichThreeNumbersAddUpTo = (
   list: Array<number>,
   target: number
-): Array<number> => [0, 0, 0];
+): Array<number> => {
+  const result: Array<number> = [];
+
+  for (let i = 0; i < list.length - 3; i++) {
+    for (let j = i + 1; j < list.length - 2; j++) {
+      for (let k = j + 1; k < list.length - 1; k++) {
+        if (list[i] + list[j] + list[k] === target) {
+          result.push(list[i]);
+          result.push(list[j]);
+          result.push(list[k]);
+        }
+      }
+    }
+  }
+
+  return result;
+};
 
 export default part2;
 export { whichThreeNumbersAddUpTo };
