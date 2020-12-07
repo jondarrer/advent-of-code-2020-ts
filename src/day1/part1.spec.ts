88636104412b98e2a923874be97271747500a38c
Jonathan Darrer <jonny@jondarrer.me.uk>
2020-12-07 12:44:19 +0000

    1-1 :sparkles: Day 1 part 1 completed!

--- src/day1/part1.spec.ts
+++ src/day1/part1.spec.ts
@@ -1,8 +1,12 @@
-import part1 from './part1';
+import part1, { whichTwoNumbersAddUpTo } from './part1';
 
 describe('day1/part1', () => {
   it('should work out which of the two numbers added together equals 2020 and return the product of the two', () => {
     const input = [1721, 979, 366, 299, 675, 1456];
     expect(part1(input)).toEqual(514579);
+  });
+  it('should work out which of the two numbers added together equals 2020', () => {
+    const input = [1721, 979, 366, 299, 675, 1456];
+    expect(whichTwoNumbersAddUpTo(input, 2020)).toEqual([1721, 299]);
   });
 });
--- src/day1/part1.ts
+++ src/day1/part1.ts
@@ -1,3 +1,25 @@
-const part1 = (expenseReport: Array<number>): number => 0;
+const part1 = (expenseReport: Array<number>): number => {
+  const twoNumbers = whichTwoNumbersAddUpTo(expenseReport, 2020);
+  return twoNumbers[0] * twoNumbers[1];
+};
+
+const whichTwoNumbersAddUpTo = (
+  list: Array<number>,
+  target: number
+): Array<number> => {
+  const result: Array<number> = [];
+
+  for (let i = 0; i < list.length - 2; i++) {
+    for (let j = i + 1; j < list.length - 1; j++) {
+      if (list[i] + list[j] === target) {
+        result.push(list[i]);
+        result.push(list[j]);
+      }
+    }
+  }
+
+  return result;
+};
 
 export default part1;
+export { whichTwoNumbersAddUpTo };
