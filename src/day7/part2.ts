ca05442439dd6067b262ec371fc6813ddc636fa8
Jonathan Darrer <jonny@jondarrer.me.uk>
2020-12-08 22:03:50 +0000

    7-2 :construction: Partially complete day 7 part 2

--- src/day7/part2.ts
+++ src/day7/part2.ts
@@ -1,4 +1,5 @@
 import { BagRegulation } from './bag-regulation';
+import { BagTreeNode } from './bag-tree-node';
 import { ContainedBag } from './contained-bag';
 
 const part2 = (regulations: Array<BagRegulation>): number => 0;
@@ -8,7 +9,21 @@ const treeOfBagsContainedBy = (
   bag: string,
   parent: ContainedBag = null
 ): BagTreeNode => {
-  return { bag: '', children: [], quantity: 0 };
+  const children: Array<BagTreeNode> = [];
+
+  const regulation: BagRegulation = regulations.find((r) => r.bag === bag);
+
+  for (let i = 0; i < regulation.contains.length; i++) {
+    const child = regulation.contains[i];
+    children.push(
+      treeOfBagsContainedBy(regulations, child.bag, {
+        bag,
+        quantity: child.quantity,
+      })
+    );
+  }
+
+  return { bag, children, quantity: parent?.quantity || 1 };
 };
 
 export default part2;
