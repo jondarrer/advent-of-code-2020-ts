0f5c074be339d8b9a6a58d9c5b875dd33f9cf4ed
Jonathan Darrer <jonny@jondarrer.me.uk>
2020-12-09 08:16:00 +0000

    9-1 :white_check_mark: Add tests for day 9 part 1

--- src/day9/part1.spec.ts
+++ src/day9/part1.spec.ts
@@ -1,14 +1,39 @@
-import part1, { somethingElse } from './part1';
+import part1, { whichNumbersSumToTarget } from './part1';
 
 describe('day9/part1', () => {
-  it('should work out the result', () => {
-    const input = [];
-    expect(part1(input)).toEqual(0);
+  it('should work out the invalid number with range of 5', () => {
+    const input = [
+      35,
+      20,
+      15,
+      25,
+      47,
+      40,
+      62,
+      55,
+      65,
+      95,
+      102,
+      117,
+      150,
+      182,
+      127,
+      219,
+      299,
+      277,
+      309,
+      576,
+    ];
+    expect(part1(input, 5)).toEqual(127);
   });
 });
-describe('day9/somethingElse', () => {
-  it('should work out something', () => {
-    const input = 0;
-    expect(somethingElse(input)).toEqual(0);
+describe('day9/whichNumbersSumToTarget', () => {
+  it('should work out that 40 is the sum of 15 and 25 from the provided nums', () => {
+    const nums = [35, 20, 15, 25, 47];
+    expect(whichNumbersSumToTarget(nums, 40)).toEqual([15, 25]);
+  });
+  it('should work out that 127 is not the sum of any of the provided nums', () => {
+    const nums = [95, 102, 117, 150, 182];
+    expect(whichNumbersSumToTarget(nums, 127)).toBeNull();
   });
 });
--- src/day9/part1.ts
+++ src/day9/part1.ts
@@ -1,6 +1,7 @@
-const part1 = (input: Array<string>): number => 0;
+const part1 = (input: Array<number>, range = 25): number => 0;
 
-const somethingElse = (arg: number): number => 0;
+const whichNumbersSumToTarget = (nums: Array<number>, target: number): number =>
+  0;
 
 export default part1;
-export { somethingElse };
+export { whichNumbersSumToTarget };
