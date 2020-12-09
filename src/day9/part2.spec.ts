6ee1718f8391d8f5e34ffa086dbf88e836dfd8c5
Jonathan Darrer <jonny@jondarrer.me.uk>
2020-12-09 12:46:27 +0000

    9-2 :white_check_mark: Add tests for day 9 part 2

--- src/day9/part2.spec.ts
+++ src/day9/part2.spec.ts
@@ -1,14 +1,36 @@
-import part2, { somethingElse } from './part2';
+import part2, { whichNumbersSumToTarget } from './part2';
+
+const input = [
+  35,
+  20,
+  15,
+  25,
+  47,
+  40,
+  62,
+  55,
+  65,
+  95,
+  102,
+  117,
+  150,
+  182,
+  127,
+  219,
+  299,
+  277,
+  309,
+  576,
+];
 
 describe('day9/part2', () => {
   it('should work out the result', () => {
     const input = [];
-    expect(part2(input)).toEqual(0);
+    expect(part2(input)).toEqual(62);
   });
 });
-describe('day9/somethingElse', () => {
+describe('day9/whichNumbersSumToTarget', () => {
   it('should work out something', () => {
-    const input = 0;
-    expect(somethingElse(input)).toEqual(0);
+    expect(whichNumbersSumToTarget(input, 127)).toEqual([15, 25, 47, 40]);
   });
 });
--- src/day9/part2.ts
+++ src/day9/part2.ts
@@ -1,6 +1,9 @@
 const part2 = (input: Array<string>): number => 0;
 
-const somethingElse = (arg: number): number => 0;
+const whichNumbersSumToTarget = (
+  nums: Array<number>,
+  target: number
+): Array<number> => [];
 
 export default part2;
-export { somethingElse };
+export { whichNumbersSumToTarget };
