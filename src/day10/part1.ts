13b44542b95c1139127616553a6cc337ed392a65
Jonathan Darrer <jonny@jondarrer.me.uk>
2020-12-10 08:16:20 +0000

    10-1 :recycle: Change parameter name to be more understandable

--- src/day10/part1.ts
+++ src/day10/part1.ts
@@ -8,16 +8,18 @@ const part1 = (input: Array<number>): number => {
 const chainAdapters = (input: Array<number>): Array<number> =>
   input.sort((a, b) => a - b);
 
-const calculateDifferences = (input: Array<number>): DifferenceTotals => {
+const calculateDifferences = (
+  chainedAdapters: Array<number>
+): DifferenceTotals => {
   const result = {};
   let last = 0;
-  for (let i = 0; i < input.length; i++) {
-    const diff = input[i] - last;
+  for (let i = 0; i < chainedAdapters.length; i++) {
+    const diff = chainedAdapters[i] - last;
     if (result[diff] === undefined) {
       result[diff] = 0;
     }
     result[diff] += 1;
-    last = input[i];
+    last = chainedAdapters[i];
   }
   return result;
 };
