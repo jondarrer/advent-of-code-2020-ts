5ae5dbd829411b34c5640edac4da35a522f7f913
Jonathan Darrer <jonny@jondarrer.me.uk>
2020-12-11 16:29:14 +0000

    11-1 :sparkles: Completed day 11 part 1!

--- src/day11/part1.spec.ts
+++ src/day11/part1.spec.ts
@@ -1,4 +1,8 @@
-import part1, { applyRulesToState } from './part1';
+import part1, {
+  applyRulesToState,
+  seatAtPosition,
+  numberOfOccupiedAdjacentSeats,
+} from './part1';
 
 const state0 = [
   'L.LL.LL.LL',
@@ -65,6 +69,19 @@ const state4 = [
   '#.#L#L#.##',
 ];
 
+const state5 = [
+  '#.#L.L#.##',
+  '#LLL#LL.L#',
+  'L.#.L..#..',
+  '#L##.##.L#',
+  '#.#L.LL.LL',
+  '#.#L#L#.##',
+  '..L.L.....',
+  '#L#L##L#L#',
+  '#.LLLLLL.L',
+  '#.#L#L#.##',
+];
+
 describe('day11/part1', () => {
   it('should work out how many occupied seats after the state becomes stable', () => {
     expect(part1(state0)).toEqual(37);
@@ -83,7 +100,41 @@ describe('day11/applyRulesToState', () => {
   it('should apply the rules to state3 to give new state4', () => {
     expect(applyRulesToState(state3)).toEqual(state4);
   });
+  it('should apply the rules to state4 to give new state5', () => {
+    expect(applyRulesToState(state4)).toEqual(state5);
+  });
   it('should apply the rules to state4 to give the same state4', () => {
-    expect(applyRulesToState(state4)).toEqual(state4);
+    expect(applyRulesToState(state5)).toEqual(state5);
+  });
+});
+describe('day11/seatAtPosition', () => {
+  it('should apply seatAtPosition 2,0 rules to state0 to give #', () => {
+    expect(seatAtPosition(state1, 2, 0)).toEqual('#');
+  });
+  it('should apply seatAtPosition 3,0 rules to state0 to give #', () => {
+    expect(seatAtPosition(state1, 3, 0)).toEqual('#');
+  });
+  it('should apply seatAtPosition 2,2 rules to state4 to give L', () => {
+    expect(seatAtPosition(state4, 2, 2)).toEqual('L');
+  });
+  it('should apply seatAtPosition 2,3 rules to state4 to give L', () => {
+    expect(seatAtPosition(state4, 2, 3)).toEqual('L');
+  });
+  it('should apply seatAtPosition 3,3 rules to state4 to give L', () => {
+    expect(seatAtPosition(state4, 3, 3)).toEqual('L');
+  });
+});
+describe('day11/numberOfOccupiedAdjacentSeats', () => {
+  it('should apply numberOfOccupiedAdjacentSeats 2,0 rules to state1', () => {
+    expect(numberOfOccupiedAdjacentSeats(state1, 2, 0)).toEqual(4);
+  });
+  it('should apply numberOfOccupiedAdjacentSeats 3,0 rules to state1', () => {
+    expect(numberOfOccupiedAdjacentSeats(state1, 3, 0)).toEqual(4);
+  });
+  it('should apply numberOfOccupiedAdjacentSeats 3,0 rules to state4', () => {
+    expect(numberOfOccupiedAdjacentSeats(state4, 3, 3)).toEqual(0);
+  });
+  it('should apply numberOfOccupiedAdjacentSeats 2,3 rules to state4', () => {
+    expect(numberOfOccupiedAdjacentSeats(state4, 2, 3)).toEqual(0);
   });
 });
--- src/day11/part1.ts
+++ src/day11/part1.ts
@@ -1,6 +1,87 @@
-const part1 = (state: Array<string>): number => 0;
+const part1 = (state: Array<string>): number => {
+  let oldState = null;
+  let newState = state;
 
-const applyRulesToState = (state: Array<string>): Array<string> => null;
+  while (!areStatesEqual(oldState, newState)) {
+    let tempState = newState;
+    newState = applyRulesToState(newState);
+    oldState = tempState;
+  }
+
+  return newState.reduce(
+    (acc, cur) => acc + (cur.match(new RegExp('#', 'g')) || []).length,
+    0
+  );
+};
+
+const areStatesEqual = (a: Array<string>, b: Array<string>): boolean => {
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+  if (a.length !== b.length) return false;
+
+  // If you don't care about the order of the elements inside
+  // the array, you should sort both arrays here.
+  // Please note that calling sort on an array will modify that array.
+  // you might want to clone your array first.
+
+  for (let i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
+};
+
+const applyRulesToState = (state: Array<string>): Array<string> => {
+  const result = [];
+
+  for (let i = 0; i < state.length; i++) {
+    const row = [];
+    for (let j = 0; j < state[i].length; j++) {
+      let seatState = state[i].charAt(j);
+      switch (seatState) {
+        case 'L': // Empty seat
+          // Are there any occupied seats immediately adjacent?
+          if (numberOfOccupiedAdjacentSeats(state, j, i) === 0) {
+            seatState = '#';
+          }
+          break;
+        case '#':
+          // Are there four or more adjacent seats which are occupied? If so, vacate it
+          if (numberOfOccupiedAdjacentSeats(state, j, i) >= 4) {
+            seatState = 'L';
+          }
+          break;
+      }
+      row.push(seatState);
+    }
+    result.push(row.join(''));
+  }
+
+  return result;
+};
+
+const numberOfOccupiedAdjacentSeats = (
+  state: Array<string>,
+  x: number,
+  y: number
+): number => {
+  return (
+    (seatAtPosition(state, x - 1, y - 1) === '#' ? 1 : 0) +
+    (seatAtPosition(state, x - 1, y) === '#' ? 1 : 0) +
+    (seatAtPosition(state, x - 1, y + 1) === '#' ? 1 : 0) +
+    (seatAtPosition(state, x, y - 1) === '#' ? 1 : 0) +
+    (seatAtPosition(state, x, y + 1) === '#' ? 1 : 0) +
+    (seatAtPosition(state, x + 1, y - 1) === '#' ? 1 : 0) +
+    (seatAtPosition(state, x + 1, y) === '#' ? 1 : 0) +
+    (seatAtPosition(state, x + 1, y + 1) === '#' ? 1 : 0)
+  );
+};
+
+const seatAtPosition = (state: Array<string>, x: number, y: number): string => {
+  if (y < 0 || y >= state.length || x < 0 || x >= state[y].length) {
+    return ' ';
+  }
+  return state[y].charAt(x);
+};
 
 export default part1;
-export { applyRulesToState };
+export { applyRulesToState, numberOfOccupiedAdjacentSeats, seatAtPosition };
