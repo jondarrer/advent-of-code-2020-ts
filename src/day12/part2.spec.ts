03e70cd6ae6db50006964a3a71141ff25caa8b0c
Jonathan Darrer <jonny@jondarrer.me.uk>
2020-12-12 18:13:30 +0000

    12-2 :sparkles: Completed day 12 part 2!

--- src/day12/part2.spec.ts
+++ src/day12/part2.spec.ts
@@ -24,8 +24,7 @@ describe('day12/moveInDirectionOfWaypoint', () => {
   it('should move in direction of waypoint 1, 10 with magnitude 10', () => {
     const waypoint = { north: 1, east: 10 };
     const ship = { north: 0, east: 0 };
-    const instruction = { action: RelativeDirection.Forward, value: 10 };
-    expect(moveInDirectionOfWaypoint(ship, waypoint, instruction)).toEqual({
+    expect(moveInDirectionOfWaypoint(ship, waypoint, 10)).toEqual({
       north: 10,
       east: 100,
     });
@@ -33,17 +32,15 @@ describe('day12/moveInDirectionOfWaypoint', () => {
   it('should move in direction of waypoint 4, 10 with magnitude 7', () => {
     const waypoint = { north: 4, east: 10 };
     const ship = { north: 10, east: 100 };
-    const instruction = { action: RelativeDirection.Forward, value: 7 };
-    expect(moveInDirectionOfWaypoint(ship, waypoint, instruction)).toEqual({
+    expect(moveInDirectionOfWaypoint(ship, waypoint, 7)).toEqual({
       north: 38,
       east: 170,
     });
   });
   it('should move in direction of waypoint 4, 11 with magnitude 11', () => {
-    const waypoint = { north: 4, east: 11 };
+    const waypoint = { north: -10, east: 4 };
     const ship = { north: 38, east: 170 };
-    const instruction = { action: RelativeDirection.Forward, value: 11 };
-    expect(moveInDirectionOfWaypoint(ship, waypoint, instruction)).toEqual({
+    expect(moveInDirectionOfWaypoint(ship, waypoint, 11)).toEqual({
       north: -72,
       east: 214,
     });
@@ -52,79 +49,74 @@ describe('day12/moveInDirectionOfWaypoint', () => {
 describe('day12/rotateWaypoint', () => {
   it('should rotate the waypoint 90 degrees clockwise from 4, 10', () => {
     const waypoint = { north: 4, east: 10 };
-    const instruction = { action: Rotation.Clockwise, value: 90 };
-    expect(rotateWaypoint(waypoint, instruction)).toEqual({
+    expect(rotateWaypoint(waypoint, Rotation.Clockwise, 90)).toEqual({
       north: -10,
       east: 4,
     });
   });
   it('should rotate the waypoint 180 degrees clockwise from 4, 10', () => {
     const waypoint = { north: 4, east: 10 };
-    const instruction = { action: Rotation.Clockwise, value: 180 };
-    expect(rotateWaypoint(waypoint, instruction)).toEqual({
+    expect(rotateWaypoint(waypoint, Rotation.Clockwise, 180)).toEqual({
       north: -4,
       east: -10,
     });
   });
   it('should rotate the waypoint 270 degrees clockwise from 4, 10', () => {
     const waypoint = { north: 4, east: 10 };
-    const instruction = { action: Rotation.Clockwise, value: 270 };
-    expect(rotateWaypoint(waypoint, instruction)).toEqual({
+    expect(rotateWaypoint(waypoint, Rotation.Clockwise, 270)).toEqual({
       north: 10,
       east: -4,
     });
   });
   it('should rotate the waypoint 90 degrees anti-clockwise from 4, 10', () => {
     const waypoint = { north: 4, east: 10 };
-    const instruction = { action: Rotation.AntiClockwise, value: 90 };
-    expect(rotateWaypoint(waypoint, instruction)).toEqual({
+    expect(rotateWaypoint(waypoint, Rotation.AntiClockwise, 90)).toEqual({
       north: 10,
       east: -4,
     });
   });
   it('should rotate the waypoint 180 degrees anti-clockwise from 4, 10', () => {
     const waypoint = { north: 4, east: 10 };
-    const instruction = { action: Rotation.AntiClockwise, value: 180 };
-    expect(rotateWaypoint(waypoint, instruction)).toEqual({
+    expect(rotateWaypoint(waypoint, Rotation.AntiClockwise, 180)).toEqual({
       north: -4,
       east: -10,
     });
   });
   it('should rotate the waypoint 270 degrees anti-clockwise from 4, 10', () => {
     const waypoint = { north: 4, east: 10 };
-    const instruction = { action: Rotation.AntiClockwise, value: 270 };
-    expect(rotateWaypoint(waypoint, instruction)).toEqual({
+    expect(rotateWaypoint(waypoint, Rotation.AntiClockwise, 270)).toEqual({
       north: -10,
       east: 4,
     });
   });
 });
-describe('day12/moveInDirectionOfWaypoint', () => {
-  it('should move in direction of waypoint 1, 10 with magnitude 10', () => {
+describe('day12/moveWaypoint', () => {
+  it('should move the waypoint at 1, 10 through north, 3 to 4, 10', () => {
     const waypoint = { north: 1, east: 10 };
-    const ship = { north: 0, east: 0 };
-    const instruction = { action: RelativeDirection.Forward, value: 10 };
-    expect(moveInDirectionOfWaypoint(ship, waypoint, instruction)).toEqual({
-      north: 10,
-      east: 100,
+    expect(moveWaypoint(waypoint, CompassDirection.North, 3)).toEqual({
+      north: 4,
+      east: 10,
     });
   });
-  it('should move in direction of waypoint 4, 10 with magnitude 7', () => {
-    const waypoint = { north: 4, east: 10 };
-    const ship = { north: 10, east: 100 };
-    const instruction = { action: RelativeDirection.Forward, value: 7 };
-    expect(moveInDirectionOfWaypoint(ship, waypoint, instruction)).toEqual({
-      north: 38,
-      east: 170,
+  it('should move the waypoint at 1, 10 through east, 3 to 1, 13', () => {
+    const waypoint = { north: 1, east: 10 };
+    expect(moveWaypoint(waypoint, CompassDirection.East, 3)).toEqual({
+      north: 1,
+      east: 13,
     });
   });
-  it('should move in direction of waypoint 4, 11 with magnitude 11', () => {
-    const waypoint = { north: 4, east: 11 };
-    const ship = { north: 38, east: 170 };
-    const instruction = { action: RelativeDirection.Forward, value: 11 };
-    expect(moveInDirectionOfWaypoint(ship, waypoint, instruction)).toEqual({
-      north: -72,
-      east: 214,
+  it('should move the waypoint at 1, 10 through south, 3 to -2, 10', () => {
+    const waypoint = { north: 1, east: 10 };
+    expect(moveWaypoint(waypoint, CompassDirection.South, 3)).toEqual({
+      north: -2,
+      east: 10,
+    });
+  });
+  it('should move the waypoint at 1, 10 through west, 3 to 1, 7', () => {
+    const waypoint = { north: 1, east: 10 };
+    expect(moveWaypoint(waypoint, CompassDirection.West, 3)).toEqual({
+      north: 1,
+      east: 7,
     });
   });
 });
--- src/day12/part2.ts
+++ src/day12/part2.ts
@@ -1,21 +1,96 @@
+import { CompassDirection } from './compass-direction';
 import { NavigationInstruction } from './navigation-instruction';
 import { Position } from './position';
+import { RelativeDirection } from './relative-direction';
+import { Rotation } from './rotation';
 
-const part2 = (input: Array<NavigationInstruction>): number => 0;
+const part2 = (instructions: Array<NavigationInstruction>): number => {
+  let waypoint = { north: 1, east: 10 };
+  let currentPosition = { north: 0, east: 0 };
+  for (let i = 0; i < instructions.length; i++) {
+    const instruction = instructions[i];
+    if (
+      instruction.action === CompassDirection.North ||
+      instruction.action === CompassDirection.East ||
+      instruction.action === CompassDirection.South ||
+      instruction.action === CompassDirection.West
+    ) {
+      waypoint = moveWaypoint(waypoint, instruction.action, instruction.value);
+    } else if (
+      instruction.action === Rotation.Clockwise ||
+      instruction.action === Rotation.AntiClockwise
+    ) {
+      waypoint = rotateWaypoint(
+        waypoint,
+        instruction.action,
+        instruction.value
+      );
+    } else if (instruction.action === RelativeDirection.Forward) {
+      currentPosition = moveInDirectionOfWaypoint(
+        currentPosition,
+        waypoint,
+        instruction.value
+      );
+    } else {
+      throw new Error(`Unexpected action '${instruction.action}'`);
+    }
+  }
+
+  return Math.abs(currentPosition.north) + Math.abs(currentPosition.east);
+};
 
 const moveInDirectionOfWaypoint = (
   ship: Position,
   waypoint: Position,
-  instruction: NavigationInstruction
-): Position => null;
+  magnitude: number
+): Position => ({
+  north: ship.north + waypoint.north * magnitude,
+  east: ship.east + waypoint.east * magnitude,
+});
+
 const rotateWaypoint = (
   waypoint: Position,
-  instruction: NavigationInstruction
-): Position => null;
+  rotation: Rotation,
+  value: number
+): Position => {
+  if (
+    (rotation === Rotation.Clockwise && value === 90) ||
+    (rotation === Rotation.AntiClockwise && value === 270)
+  ) {
+    return { north: -1 * waypoint.east, east: waypoint.north };
+  } else if (
+    (rotation === Rotation.Clockwise && value === 180) ||
+    (rotation === Rotation.AntiClockwise && value === 180)
+  ) {
+    return { north: -1 * waypoint.north, east: -1 * waypoint.east };
+  } else if (
+    (rotation === Rotation.Clockwise && value === 270) ||
+    (rotation === Rotation.AntiClockwise && value === 90)
+  ) {
+    return { north: waypoint.east, east: -1 * waypoint.north };
+  } else {
+    throw new Error(`Unexpected rotation '${rotation}' and value '${value}'`);
+  }
+};
+
 const moveWaypoint = (
   waypoint: Position,
-  instruction: NavigationInstruction
-): Position => null;
+  direction: CompassDirection,
+  value: number
+): Position => {
+  switch (direction) {
+    case CompassDirection.North:
+      return { north: waypoint.north + value, east: waypoint.east };
+    case CompassDirection.East:
+      return { north: waypoint.north, east: waypoint.east + value };
+    case CompassDirection.South:
+      return { north: waypoint.north - value, east: waypoint.east };
+    case CompassDirection.West:
+      return { north: waypoint.north, east: waypoint.east - value };
+    default:
+      throw new Error(`Unexpected direction '${direction}'`);
+  }
+};
 
 export default part2;
 export { moveInDirectionOfWaypoint, rotateWaypoint, moveWaypoint };
