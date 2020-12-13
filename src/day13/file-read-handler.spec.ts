d21c5d609e62e240393b4f63a12e6c21eff9673b
Jonathan Darrer <jonny@jondarrer.me.uk>
2020-12-13 08:37:22 +0000

    13-0 :construction: Input file read for day 13

--- src/day13/file-read-handler.spec.ts
+++ src/day13/file-read-handler.spec.ts
@@ -2,11 +2,11 @@ import fileReadHandler from './file-read-handler';
 
 describe('day13/file-read-handler', () => {
   it('should read in strings and convert them to numbers', () => {
-    const lines = ['1721', '979', '366', '299', '675', '1456'];
+    const lines = ['939', '7,13,x,x,59,x,31,19'];
     const accumulator = [];
     lines.forEach((line) => {
       fileReadHandler(accumulator, line);
     });
-    expect(accumulator).toEqual(['1721', '979', '366', '299', '675', '1456']);
+    expect(accumulator).toEqual([939, [7, 13, 'x', 'x', 59, 'x', 31, 19]]);
   });
 });
--- src/day13/file-read-handler.ts
+++ src/day13/file-read-handler.ts
@@ -1,5 +1,16 @@
-const fileReadHandler = (accumulator: Array<string>, line: string): void => {
-  accumulator.push(line);
+const fileReadHandler = (
+  accumulator: Array<number | Array<number | string>>,
+  line: string
+): void => {
+  let result;
+  if (line.indexOf(',') === -1) {
+    result = parseInt(line);
+  } else {
+    result = line
+      .split(',')
+      .map((value) => (value === 'x' ? value : parseInt(value)));
+  }
+  accumulator.push(result);
 };
 
 export default fileReadHandler;
