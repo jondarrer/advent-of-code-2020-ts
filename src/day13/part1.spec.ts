fb231e4c46a9922c2d7b9e04fe7bce93833b2f67
Jonathan Darrer <jonny@jondarrer.me.uk>
2020-12-13 08:37:39 +0000

    13-1 :white_check_mark: Added tests for day 13 part 1

--- src/day13/part1.spec.ts
+++ src/day13/part1.spec.ts
@@ -1,14 +1,25 @@
-import part1, { somethingElse } from './part1';
+import part1, { arrivesAtOrAfter } from './part1';
 
 describe('day13/part1', () => {
   it('should work out the result', () => {
-    const input = [];
-    expect(part1(input)).toEqual(0);
+    const input = [939, [7, 13, 'x', 'x', 59, 'x', 31, 19]];
+    expect(part1(input)).toEqual(295);
   });
 });
-describe('day13/somethingElse', () => {
-  it('should work out something', () => {
-    const input = 0;
-    expect(somethingElse(input)).toEqual(0);
+describe('day13/arrivesAtOrAfter', () => {
+  it('should work out period 7 on or after 939 is 945', () => {
+    expect(arrivesAtOrAfter(7, 939)).toEqual(945);
+  });
+  it('should work out period 13 on or after 939 is 949', () => {
+    expect(arrivesAtOrAfter(13, 939)).toEqual(949);
+  });
+  it('should work out period 59 on or after 939 is 944', () => {
+    expect(arrivesAtOrAfter(59, 939)).toEqual(944);
+  });
+  it('should work out period 31 on or after 939 is 961', () => {
+    expect(arrivesAtOrAfter(31, 939)).toEqual(961);
+  });
+  it('should work out period 31 on or after 939 is 950', () => {
+    expect(arrivesAtOrAfter(31, 939)).toEqual(950);
   });
 });
--- src/day13/part1.ts
+++ src/day13/part1.ts
@@ -1,6 +1,6 @@
-const part1 = (input: Array<string>): number => 0;
+const part1 = (input: Array<number | Array<number | string>>): number => 0;
 
-const somethingElse = (arg: number): number => 0;
+const arrivesAtOrAfter = (period: number, target: number): number => 0;
 
 export default part1;
-export { somethingElse };
+export { arrivesAtOrAfter };
