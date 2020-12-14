ac914fa35566baca781fcc206dc717f618985fa0
Jonathan Darrer <jonny@jondarrer.me.uk>
2020-12-14 07:52:45 +0000

    13-2 :construction: Partially solved day 13 part 2

--- src/day13/part2.spec.ts
+++ src/day13/part2.spec.ts
@@ -1,12 +1,12 @@
-import part2 from './part2';
+import part2, { calculateOriginFactor } from './part2';
 
 describe('day13/part2', () => {
   it('should work out the result for [7, 13, "x", "x", 59, "x", 31, 19] to be 1068781', () => {
-    const input = [939, [7, 13, 'x', 'x', 59, 'x', 31, 19]];
+    const input = [-1, [7, 13, 'x', 'x', 59, 'x', 31, 19]];
     expect(part2(input)).toEqual(1068781);
   });
   it('should work out the result for [17, "x", 13, 19] to be 3417', () => {
-    const input = [939, [17, 'x', 13, 19]];
+    const input = [-1, [17, 'x', 13, 19]];
     expect(part2(input)).toEqual(3417);
   });
   it('should work out the result for [67, 7, 59, 61] to be 754018', () => {
@@ -22,7 +22,45 @@ describe('day13/part2', () => {
     expect(part2(input)).toEqual(1261476);
   });
   it('should work out the result for [1789, 37, 47, 1889] to be 1202161486', () => {
-    const input = [939, [1789, 37, 47, 1889]];
+    const input = [-1, [1789, 37, 47, 1889]];
     expect(part2(input)).toEqual(1202161486);
+  });
+  it('should work out the result for [3, 5, "x", 7] to be 39', () => {
+    const input = [-1, [3, 5, 'x', 7]];
+    expect(part2(input)).toEqual(39);
+  });
+  it('should work out the result for [3, "x", 5, 7] to be 18', () => {
+    const input = [-1, [3, 'x', 5, 7]];
+    expect(part2(input)).toEqual(18);
+  });
+  it('should work out the result for [3, 7, "x", 5] to be 18', () => {
+    const input = [-1, [3, 7, 'x', 5]];
+    expect(part2(input)).toEqual(27);
+  });
+});
+describe('day13/calculateOriginFactor', () => {
+  it('should calculate a factor of 3 for origin 3 against other 5 with offset 1', () => {
+    expect(calculateOriginFactor(3, 5, 1)).toEqual(3);
+  });
+  it('should calculate a factor of 1 for origin 3 against other 5 with offset 2', () => {
+    expect(calculateOriginFactor(3, 5, 2)).toEqual(1);
+  });
+  it('should calculate a factor of 4 for origin 3 against other 5 with offset 3', () => {
+    expect(calculateOriginFactor(3, 5, 3)).toEqual(4);
+  });
+  it('should calculate a factor of 2 for origin 3 against other 7 with offset 1', () => {
+    expect(calculateOriginFactor(3, 7, 1)).toEqual(2);
+  });
+  it('should calculate a factor of 4 for origin 3 against other 7 with offset 2', () => {
+    expect(calculateOriginFactor(3, 7, 2)).toEqual(4);
+  });
+  it('should calculate a factor of 6 for origin 3 against other 7 with offset 3', () => {
+    expect(calculateOriginFactor(3, 7, 3)).toEqual(6);
+  });
+  it('should calculate a factor of 6 for origin 17 against other 13 with offset 2', () => {
+    expect(calculateOriginFactor(17, 13, 2)).toEqual(6);
+  });
+  it('should calculate a factor of 11 for origin 17 against other 19 with offset 3', () => {
+    expect(calculateOriginFactor(17, 19, 3)).toEqual(11);
   });
 });
--- src/day13/part2.ts
+++ src/day13/part2.ts
@@ -1,3 +1,19 @@
 const part2 = (input: Array<number | Array<number | string>>): number => 0;
 
+const calculateOriginFactor = (
+  origin: number,
+  other: number,
+  offset: number
+): number => {
+  if (origin === offset) return other - 1;
+
+  let factor = 1;
+  while ((origin * factor + offset) % other !== 0) {
+    factor += 1;
+  }
+
+  return factor;
+};
+
 export default part2;
+export { calculateOriginFactor };
