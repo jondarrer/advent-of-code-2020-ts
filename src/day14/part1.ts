8ff49d33a22db314d5019ae2b48b63d07ad5fa2c
Jonathan Darrer <jonny@jondarrer.me.uk>
2020-12-14 08:55:27 +0000

    14-1 :construction: Partially completed day 14 part 1

--- src/day14/part1.ts
+++ src/day14/part1.ts
@@ -2,9 +2,23 @@ import { MaskedWritesInstruction } from './masked-writes-instruction';
 
 const part1 = (instructions: Array<MaskedWritesInstruction>): number => 0;
 
-const applyMaskToValue = (mask: string, value: string): string => null;
+const applyMaskToValue = (mask: string, value: string): string => {
+  let result = [];
 
-const padValue = (value: string, length: number, char: string): string => null;
+  for (let i = 0; i < mask.length; i++) {
+    const char = mask[i];
+    if (char !== 'X') {
+      result[i] = char;
+    } else {
+      result[i] = value.charAt(i);
+    }
+  }
+
+  return result.join('');
+};
+
+const padValue = (value: string, length: number, char: string): string =>
+  value.padStart(length, char);
 
 export default part1;
 export { applyMaskToValue, padValue };
