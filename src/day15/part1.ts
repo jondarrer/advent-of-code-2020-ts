b774022c4ae1cbc1e21839517f4f79e4e535cdc6
Jonathan Darrer <jonny@jondarrer.me.uk>
2020-12-15 07:29:04 +0000

    15-1 :sparkles: Completed day 15 part 1

--- src/day15/part1.ts
+++ src/day15/part1.ts
@@ -1,6 +1,20 @@
-const part1 = (input: Array<number>): number => 0;
+const part1 = (input: Array<number>): number => {
+  while (input.length < 2020) {
+    input.push(nextNumber(input));
+  }
 
-const nextNumber = (input: Array<number>): number => 0;
+  return input[input.length - 1];
+};
+
+const nextNumber = (input: Array<number>): number => {
+  const lastNumber = input[input.length - 1];
+
+  const lastNumberIndex = input
+    .slice(0, input.length - 1)
+    .lastIndexOf(lastNumber);
+
+  return lastNumberIndex === -1 ? 0 : input.length - lastNumberIndex - 1;
+};
 
 export default part1;
 export { nextNumber };
