d3ef84ab3aa65cb3e3555fef3ca52764450d756e
Jonathan Darrer <jonny@jondarrer.me.uk>
2020-12-17 20:37:54 +0000

    16-1 :sparkles: Completed day 16 part 1!

--- src/day16/part1.ts
+++ src/day16/part1.ts
@@ -1,12 +1,50 @@
 import { Notes } from './notes';
 import { Rule } from './rule';
 
-const part1 = (input: Array<Notes>): number => 0;
+const part1 = (input: Array<Notes>): number => {
+  const result = [];
+  const notes = input[0];
+
+  for (let i = 0; i < notes['nearby-tickets'].length; i++) {
+    result.push(
+      ...invalidTicketFieldsForRules(notes['nearby-tickets'][i], notes['rules'])
+    );
+  }
+
+  return result.reduce((acc, cur) => acc + cur, 0);
+};
 
 const invalidTicketFieldsForRules = (
   ticket: Array<number>,
   rules: Array<Rule>
-): Array<number> => null;
+): Array<number> => {
+  const result = [];
+  let isValid;
+
+  for (let i = 0; i < ticket.length; i++) {
+    isValid = false;
+    const field = ticket[i];
+    for (let j = 0; j < rules.length; j++) {
+      // Is the field valid?
+      if (isFieldValidForRanges(field, rules[j])) {
+        isValid = true;
+        break;
+      }
+    }
+    if (!isValid) {
+      result.push(field);
+    }
+  }
+
+  return result;
+};
+
+const isFieldValidForRanges = (field: number, rule: Rule): boolean => {
+  return (
+    (field >= rule.ranges[0].min && field <= rule.ranges[0].max) ||
+    (field >= rule.ranges[1].min && field <= rule.ranges[1].max)
+  );
+};
 
 export default part1;
-export { invalidTicketFieldsForRules };
+export { invalidTicketFieldsForRules, isFieldValidForRanges };
